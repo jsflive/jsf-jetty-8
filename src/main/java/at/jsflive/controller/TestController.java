df1975af12fb3449eeb0c70b78c2d9f3235a274b
Michael Kurz <michael.kurz@irian.at>
2012-08-19 21:02:34 +0200

    initial version

---
+++ src/main/java/at/jsflive/controller/TestController.java
@@ -0,0 +1,24 @@
+package at.jsflive.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class TestController {
+    private boolean collapsed = false;
+
+    public boolean isCollapsed() {
+        return collapsed;
+    }
+
+    public void setCollapsed(boolean collapsed) {
+        this.collapsed = collapsed;
+    }
+
+    public String toggle() {
+        collapsed = !collapsed;
+        return null;
+    }
+
+}
